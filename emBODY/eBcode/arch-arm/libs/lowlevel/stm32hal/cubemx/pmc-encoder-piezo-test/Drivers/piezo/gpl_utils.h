97bf3f4962f994f4da41eeb0c74776c1b8e47eb5
Andrea Merello <andrea.merello@gmail.com>
2020-10-26 08:48:58 +0100

    kill unused variable in ACCESS_ONCE() marco

--- emBODY/eBcode/arch-arm/libs/lowlevel/stm32hal/cubemx/pmc-encoder-piezo-test/Drivers/piezo/gpl_utils.h
+++ emBODY/eBcode/arch-arm/libs/lowlevel/stm32hal/cubemx/pmc-encoder-piezo-test/Drivers/piezo/gpl_utils.h
@@ -2,10 +2,7 @@
 #define __GPL_UTILS_H__
 
 /* all this stuff is stolen from, or based on code from, the Linux kernel */
-#define __ACCESS_ONCE(x) ({		 \
-	typeof(x) __var = (typeof(x)) 0; \
-	(volatile typeof(x) *)&(x); })
-#define ACCESS_ONCE(x) (*__ACCESS_ONCE(x))
+#define ACCESS_ONCE(x) (*(volatile typeof(x) *)&(x))
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
 
 int fls(unsigned int x);
