9e16cbd0c00a2869548cca3e803cf83b9d022b7a
Andrea Merello <andrea.merello@gmail.com>
2020-11-05 11:13:46 +0100

    add missing gpio in ARM hal wrapper

--- emBODY/eBcode/arch-arm/libs/lowlevel/stm32hal/cubemx/pmc-encoder-piezo-test/Drivers/piezo/hal.h
+++ emBODY/eBcode/arch-arm/libs/lowlevel/stm32hal/cubemx/pmc-encoder-piezo-test/Drivers/piezo/hal.h
@@ -5,6 +5,10 @@
 #include <stm32g4xx.h>
 /* for GPIO pins definitions */
 #include "../../Inc/main.h"
+#include "../../Inc/spi.h"
+#include "../../Inc/comp.h"
+#include "../../Inc/gpio.h"
 #include "stm32g4xx_hal_comp.h"
 #include "stm32g4xx_hal_rcc.h"
+
 #endif
