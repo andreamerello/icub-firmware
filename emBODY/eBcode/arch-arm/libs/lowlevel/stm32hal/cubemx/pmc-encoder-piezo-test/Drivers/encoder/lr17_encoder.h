922cb3d6ba5ce94d3cabf002f2da50caa91f2395
Andrea Merello <andrea.merello@gmail.com>
2020-11-10 12:51:19 +0100

    add exported API prototypes to lr17_encoder header

--- emBODY/eBcode/arch-arm/libs/lowlevel/stm32hal/cubemx/pmc-encoder-piezo-test/Drivers/encoder/lr17_encoder.h
+++ emBODY/eBcode/arch-arm/libs/lowlevel/stm32hal/cubemx/pmc-encoder-piezo-test/Drivers/encoder/lr17_encoder.h
@@ -6,4 +6,8 @@
 #include "../gpl_utils.h"
 #include "hal.h"
 
+extern bool lr17_encoder_init();
+extern bool lr17_encoder_acquire(void(*cb)(void *arg), void *arg);
+extern bool lr17_encoder_get(int *angle);
+
 #endif
