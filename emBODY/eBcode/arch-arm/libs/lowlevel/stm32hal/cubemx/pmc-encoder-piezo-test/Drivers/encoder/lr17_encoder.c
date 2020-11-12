7f2fd41fe7cca87cb558fc48a773634c49eee547
Andrea Merello <andrea.merello@gmail.com>
2020-11-12 12:11:42 +0100

    drop timing constraint checks from SPI encoder driver; caller will take care of that

--- emBODY/eBcode/arch-arm/libs/lowlevel/stm32hal/cubemx/pmc-encoder-piezo-test/Drivers/encoder/lr17_encoder.c
+++ emBODY/eBcode/arch-arm/libs/lowlevel/stm32hal/cubemx/pmc-encoder-piezo-test/Drivers/encoder/lr17_encoder.c
@@ -10,38 +10,22 @@ union {
     uint16_t val;
 } lr17_encoder_buf;
 
-int lr17_encoder_time;
-
-
 bool lr17_encoder_init()
 {
-#warning FIXME_dummy
-	lr17_encoder_time = 0;
+    return true;
 }
 
 bool lr17_encoder_acquire(void(*cb)(void *arg), void *arg)
 {
-    int time;
-
     lr17_encoder_cb_arg = arg;
     lr17_encoder_cb = cb;
 
-#warning FIXME_dummy
-    time = 501;
-
-    if ((lr17_encoder_time - time) > 500) {
-        HAL_SPI_Receive_IT(&hspi4, lr17_encoder_buf.spi, 2);
-        return true;
-    } else {
-        return false;
-    }
+    HAL_SPI_Receive_IT(&hspi4, lr17_encoder_buf.spi, 2);
+    return true;
 }
 
 void lr17_spi_cb(SPI_HandleTypeDef *spi)
 {
-#warning FIXME_dummy
-    lr17_encoder_time = 0;
-
     ACCESS_ONCE(lr17_encoder_val) =
         ACCESS_ONCE(lr17_encoder_buf.val) & 0x7fff;
     if (lr17_encoder_cb)
